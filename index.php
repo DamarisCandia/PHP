8f8ff2d3a701a6b9c1b6efe253de4e2419c3d780
Damaris Candia <damaris.candia1701@alumnos.ubiobio.cl>
2023-05-06 18:47:16 -0400

    Se cambia el index para tener un login en el, se crea inicio_cliente que corresponde al antiguo index.php

--- index.php
+++ index.php
@@ -12,112 +12,24 @@ include 'conexionBDD.php'
     
 </head>
 <body>
-    <div id = "info">
+    <img src="css/logoc.png">
 
-    <header>
-    <h1>Vive Feliz</h1>
-    </header>
+    <div id="login", class="bloque5">
 
-    <main>
-        <div id = "list">
-            <nav>
-                <ul>
-                    <li><a href="?seccion=seccion1">Gastos</a></li>
-                    <li><a href="?seccion=seccion2">Pagos</a></li>
-                    <li><a href="?seccion=seccion3">Historia de pagos</a></li>
-                </ul>
-            </nav>
+        <div class="input">
+            <label for="uname">Usuario: </label>
+            <input type="text" id="uname" name="name">
+        </div>
+        
+        <div class="input">
+            <label for="uname">Contraseña: </label>
+            <input type="password" id="uname" name="name">
         </div>
 
-        <?php
-            if (isset($_GET['seccion']) && $_GET['seccion'] == 'seccion1') {
-            ?>
-                <div class="seccion">
-                <section id="1">
-            <h2>Gastos</h2>
-            
-            <table class = "table">
-                <tr>
-                    <td>
-                        Gastos
-                    </td>
-                    <td>
-                        Valor
-                    </td>
-                </tr>
-                <?php
-                $query = $bdd->query("SELECT * FROM gastos");
-                $conta =0;
-                if($query->num_rows > 0){
-                    while($row = $query->fetch_assoc()){
-                        $conta = $conta + $row["valor"];
-                ?>
-                <tr>
-                <td scope="row"><?php echo $row["tipo"]?></td>
-                <td scope="row"><?php echo $row["valor"]?></td>
-                </tr>
-
-                <?php
-                } 
-                }else{
-                    ?>
-                    <p>No hay perritos</p>
-                    <?php
-                }
-                ?>
-            </table>
-            
-        </section>
-                </div>
-            <?php
-            }
-            elseif (isset($_GET['seccion']) && $_GET['seccion'] == 'seccion2') {
-            ?>  
-                    <section id="2">
-                        <h2>Pagos</h2>
-                        <br>
-                        <p>Fecha vencimiento : 02/05/2023</p>
-                        <h4>Total: $65.000</h4>
-                        <p>En caso de atrasarse en su pago se cobrara una multa de $5.000 por semana</p>
-                        <br>
-                        <div id="boton">
-                            <button> Pagar</button>
-                        </div>
-                    </section>
-            <?php
-            }
-            elseif (isset($_GET['seccion']) && $_GET['seccion'] == 'seccion3') {
-            ?>
-                <div class="seccion">
-                <section id="3">
-                    <h2>Historial de Pagos</h2>
-
-                    <ul>
-                        <li>Abril : Cancelado</li>
-                        <li>Marzo : Cancelado</li>
-                        <li>Febrero : Cancelado</li>
-                        <li>Enero : Cancelado</li>
-                        <li>Diciembre : Cancelado</li>
-                    </ul>
-
-                    <div id="boton">
-                            <button>Generar informe</button>
-                        </div>
-                </section>
-                </div>
-            <?php
-            }
-            ?>
-
-
-        
-
-        
-
-        
-    </main>
+        <div class="boton">
+            <button onclick="location='inicio_cliente.php'">Iniciar Sesión</button>
+        </div>
 
     </div>
-    
 </body>
 </html>
