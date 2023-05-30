4c2037e63005e21331ce26f99b1cc681b6efe6d6
Jose Contreras <jcontreras@kpitec.com>
2023-05-30 19:09:37 -0400

    gastos

--- PHP_ADMIN/Consultar_Pagos.php
+++ PHP_ADMIN/Consultar_Pagos.php
@@ -1,37 +1,45 @@
-<?php include("template/cabecera.php");?>
+<?php
+include("template/cabecera.php");
+include('../conexionBDD.php');
+?>
 
 <div class="container text-center">
   <h2>Tabla de Pagos</h2>
   <table class="table table-striped table-hover">
     <thead>
       <tr>
-        <th scope="col">Número de vivienda</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Valor</th>
         <th scope="col">Fecha</th>
-        <th scope="col">Monto</th>
         <th scope="col">Estado</th>
+        <th scope="col">Num° depto</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>001</td>
-        <td>10/05/2023</td>
-        <td>$500</td>
-        <td>Pendiente</td>
-      </tr>
-      <tr>
-        <td>002</td>
-        <td>05/05/2023</td>
-        <td>$250</td>
-        <td>Pagado</td>
-      </tr>
-      <tr>
-        <td>003</td>
-        <td>02/05/2023</td>
-        <td>$1000</td>
-        <td>Pendiente</td>
-      </tr>
+      <?php
+       $query = $bdd->query("SELECT Gasto_Nombre, Gasto_Valor, Gasto_Fecha, Gasto_Estado_Id, Dept_Num_Id FROM VF_Gastos");
+      $conta = 0;
+      if ($query->num_rows > 0) {
+        while ($row = $query->fetch_assoc()) {
+          $estadoQuery = $bdd->query("SELECT Tipo_Estado FROM VF_Estado_Gasto WHERE Estado_Id = '".$row["Gasto_Estado_Id"]."'");
+          $estadoRow = $estadoQuery->fetch_assoc();
+          $conta = $conta + 1;
+      ?>
+          <tr>
+            <td scope="row"><?php echo $row["Gasto_Nombre"]; ?></td>
+            <td><?php echo $row["Gasto_Valor"]; ?></td>
+            <td><?php echo $row["Gasto_Fecha"]; ?></td>
+            <td><?php echo $estadoRow["Tipo_Estado"]; ?></td>
+            <td><?php echo $row["Dept_Num_Id"]; ?></td>
+          </tr>
+      <?php
+        }
+      } else {
+        echo "<tr><td colspan='5'>No hay gastos</td></tr>";
+      }
+      ?>
     </tbody>
   </table>
 </div>
 
-<?php include("template/pie.php");?>
+<?php include("template/pie.php"); ?>
--- guardar_gastos.php
+++ guardar_gastos.php
@@ -1,19 +1,45 @@
 <?php
-    include 'conexionBDD.php';
+include 'conexionBDD.php';
 
-    if(isset($_POST['tipo']) && isset($_POST['valor'])){
-        $tipo = $_POST['tipo'];
-        $valor = $_POST['valor'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $tipoGasto = $_POST["tipo_gasto"];
+    $valorGasto = $_POST["valor_gasto"];
+    $fechaGasto = $_POST["fecha_gasto"];
+    $edificioId = $_POST["edificio"];
 
-        $stmt = $bdd->prepare("INSERT INTO gastos (tipo, valor) VALUES (?, ?)");
-        $stmt->bind_param("si", $tipo, $valor);
-
-        if($stmt->execute()){
-            echo "Gasto guardado exitosamente";
-        }else{
-            echo "Error al guardar el gasto";
-        }
-
-        $stmt->close();
+    // Obtener el último Gasto_Id de la tabla VF_Gastos
+    $queryGastosId = $bdd->query("SELECT Gasto_Id FROM VF_Gastos ORDER BY Gasto_Id DESC LIMIT 1");
+    if ($queryGastosId->num_rows > 0) {
+        $rowGastosId = $queryGastosId->fetch_assoc();
+        $gastosId = $rowGastosId["Gasto_Id"] + 1;
+    } else {
+        $gastosId = 1;
     }
+
+    // Insertar el gasto en la tabla VF_Gastos
+    $insertGasto = $bdd->prepare("INSERT INTO VF_Gastos (Gasto_Id, Gasto_Nombre, Gasto_Valor, Gasto_Fecha, Gasto_Estado_Id, Dept_Num_Id) VALUES (?, ?, ?, ?, ?, ?)");
+
+    // Obtener todos los departamentos del edificio seleccionado
+    $queryDepartamentos = $bdd->prepare("SELECT Dept_Num_Id FROM VF_Departamentos WHERE Edif_Id = ?");
+    $queryDepartamentos->bind_param("i", $edificioId);
+    $queryDepartamentos->execute();
+    $resultDepartamentos = $queryDepartamentos->get_result();
+
+    if ($resultDepartamentos->num_rows > 0) {
+        while ($rowDepartamento = $resultDepartamentos->fetch_assoc()) {
+            $deptNumId = $rowDepartamento["Dept_Num_Id"];
+            $gastoEstadoId = 1; // Asignar el estado de gasto correspondiente
+
+            $insertGasto->bind_param("isdisi", $gastosId, $tipoGasto, $valorGasto, $fechaGasto, $gastoEstadoId, $deptNumId);
+            $insertGasto->execute();
+        }
+        $queryDepartamentos->close();
+        $insertGasto->close();
+        echo "El gasto se ha registrado para todos los departamentos del edificio seleccionado.";
+    } else {
+        echo "No se encontraron departamentos para el edificio seleccionado.";
+    }
+} else {
+    echo "No se recibieron los datos del formulario correctamente.";
+}
 ?>
