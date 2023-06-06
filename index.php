9f31e036f2d86f0f6ac94d4afc8a24741353f195
Damaris Candia <damaris.candia1701@alumnos.ubiobio.cl>
2023-06-06 16:20:34 -0400

    Se cambia index por el index2.0, se elimina el index2.0 que no estaba en uso

--- index.php
+++ index.php
@@ -1,6 +1,50 @@
 <?php
-include 'conexionBDD.php'
+include 'conexionBDD.php';
+include("template/cabecera.php");
 ?>
+
+<style>
+  .form-group {
+    width: 300px;
+  }
+   .form-group label {
+    display: block;
+  }
+   button{
+    width: 270px;
+  }
+  img{
+    margin-top: 10px;
+    max-width: 300px;
+    max-height: 300px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: -50px;
+}
+.container {
+  margin-top: -160px;
+}
+
+#login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  margin-bottom: 200px;
+}
+
+.input {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.input label {
+  margin-bottom: 10px;
+  text-align: center;
+}
+</style>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -8,7 +52,7 @@ include 'conexionBDD.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gastos Comunes</title>
-    <link rel="stylesheet" href="css/Style.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
 </head>
 <body>
@@ -26,12 +70,14 @@ include 'conexionBDD.php'
             <input type="password" id="uname" name="name">
         </div>
 
+        <br>
 
-
-        <div class="boton">
-            <button  type="submit" class="btn btn-primary btn-lg"  onclick="location='./PHP_ADMIN/index2_0.php'">2.0</button>
-        </div>
+        <button type="submit" class="btn btn-primary btn-lg" onclick="location='inicio_cliente.php'">Ingresar</button>
+        <button type="submit" class="btn btn-primary btn-lg" onclick="location='admin.php'">¿Olvidaste tu contraseña?</button>
+        <button  type="submit" class="btn btn-primary btn-lg"  onclick="location='./PHP_ADMIN/index2_0.php'">2.0</button>
 
     </div>
+
+    <?php include("template/pie.php");?>
 </body>
 </html>
---
+++ template/cabecera.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/diseño.css" type="text/css">
+        <link rel="stylesheet" href="../css/carousel.css" type="text/css">
+        <link rel="stylesheet" href="../css/carousel.rtl.css" type="text/css">
+        <link rel="icon" type="image/x-icon" href="../css/logoc.ico">
+
+        <script src="js/bootstrap.modal.js"></script>
+        
+        <title>VIVE FELIZ</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index2_0.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Ingresar_gastos.php"> Ingresar gastos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Consultar_Pagos.php"> Consultar pagos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Generar_Informe.php"> Generar informes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Galeria_Imagenes.php">Galeria</a>
+                </li>
+            </ul>
+        </nav>
+    </body>
+</html>
